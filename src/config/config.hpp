b83e9d03fbbf13dd8b1420c75931d4b14a846325
PreethamVJ <preethamvj2005@gmail.com>
2025-02-01 10:18:17 +0000

    fix: comment

--- src/config/config.hpp
+++ src/config/config.hpp
@@ -1,26 +1,32 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
-#include <filesystem>
-#include <vector>
-#include <string>
-#include <fstream>
-#include <iostream>
-#include <yaml-cpp/yaml.h>
+#include <filesystem>// For handling file system paths
+#include <vector>// For using std::vector
+#include <string>// For using std::string
+#include <fstream>// For file handling operations
+#include <iostream>// For input/output operations
+#include <yaml-cpp/yaml.h>// For parsing YAML configuration files
 
 using namespace std;
 
-using namespace std::filesystem;
+using namespace std::filesystem; // Allows direct use of filesystem classes
+
+// Enumeration for logging levels
 enum  LogLevel {
     INFO,  
     WARN, 
     ERROR, 
     DEBUG  
 };
+
+// Enumeration for supported hashing algorithms
 enum  Algorithm {
     MD5,    
     SHA256,
     NONE
 };
+
+// Configuration class for managing settings from a YAML file
 class Config {
 private:
     YAML::Node config;
@@ -33,7 +39,10 @@ private:
     LogLevel log_level;
     Algorithm algorithm; 
 public:
+    // Parses the configuration file and loads settings
     void parse(const string&);
+
+    // Getters for configuration parameters
     vector<path> get_paths();
     int get_interval();
     bool get_noti_enabled();
@@ -44,5 +53,5 @@ public:
     Algorithm get_algorithm();
 };
 
-#endif
+#endif // CONFIG_HPP
 
