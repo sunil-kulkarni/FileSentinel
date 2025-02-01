8dcb32c8d6229b9484b4b3f5446f48452ea991fb
pushkar-gr <gr.pushkar.7@gmail.com>
2025-02-01 22:13:05 +0530

    refactor: checksum.hpp

--- src/checksum/checksum.hpp
+++ src/checksum/checksum.hpp
@@ -6,12 +6,26 @@
 #include <sstream>
 #include "../config/config.hpp"
 
+using namespace std;
+using namespace std::filesystem;
+
 #ifndef CHECKSUM_HPP
 #define CHECKSUM_HPP
 
 class Checksum {
 private:
   Config *config;
+  
+  //Computes the SHA-256 checksum of a given file and stores it in 'str'. 
+  //Returns true if successful, false if an error occurs (e.g., file not found)
+  bool compute_checksum_sha(const path&, string *);
+
+  //Computes the MD5 checksum of a given file and stores it in 'str'. 
+  //Returns true if successful, false if an error occurs (e.g., file not found)
+  bool compute_checksum_md(const path&, string *);
+
+  //Reads the string from filepath and returns the string
+  bool none(const path&, string *);
 
 public:
   Checksum(Config *); //constructor
@@ -33,6 +47,9 @@ public:
 
   //destructor
   ~Checksum();
+  //computes checksum based on algo in config
+  bool compute_checksum(const path&, string *);
+
 };
 
-#endif // CHECKSUM_HPP
+#endif
