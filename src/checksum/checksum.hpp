2a986654972160618056d8317f71634556f90f7f
Sunil <sunildku9@gmail.com>
2025-02-01 15:49:50 +0530

    add: comments to checksum.hpp

--- src/checksum/checksum.hpp
+++ src/checksum/checksum.hpp
@@ -17,5 +17,15 @@ public:
     bool computeChecksum_sha(const std::string& filePath, std::string* str);
     bool compareChecksum(const std::string& filePath, const std::string *);
     static bool computeChecksum_md(const std::string &filePath, std::string *str);
+
+    /* Computes the SHA-256 checksum of a given file and stores it in 'str'. Returns true if successful, false if an error occurs (e.g., file not found).*/
+bool computeChecksum_sha(const std::string& filePath, std::string* str);
+
+/*Compares the computed SHA-256 checksum of a file with a given checksum. Returns true if they match, false otherwise.*/
+bool compareChecksum(const std::string& filePath, const std::string *givenChecksum);
+
+/*Computes the MD5 checksum of a given file and stores it in 'str'. Returns true if successful, false if an error occurs (e.g., file not found).*/
+static bool computeChecksum_md(const std::string &filePath, std::string *str);
+
 };
 #endif
